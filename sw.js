4de5a5c59ebacb6d76362722c5fa462ceacf1690
paytonhussey76-rgb <paytonhussey76@gmail.com>
2026-09-08 18:12:40 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -5,7 +5,7 @@
 // even after this worker updated. HTML now goes to the network first
 // and only falls back to cache when there is no signal.
 
-const CACHE = 'loc3-direct-v1';
+const CACHE = 'loc3-direct-v2';
 
 const LOCAL = [
   './',
